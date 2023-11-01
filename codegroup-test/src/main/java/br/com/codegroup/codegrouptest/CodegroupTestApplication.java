c85ebba4405124f2f2a1be9db65500635857b059
tec <gilsonnetotec@gmail.com>
2023-11-01 01:50:18 -0300

    feat: inicio de projeto

---
+++ codegroup-test/src/main/java/br/com/codegroup/codegrouptest/CodegroupTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.codegroup.codegrouptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegroupTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegroupTestApplication.class, args);
+	}
+
+}
