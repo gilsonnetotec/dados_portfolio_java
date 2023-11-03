b7ef2ad8ed94e2ac5450b956e8831773d06c4b20
tec <gilsonnetotec@gmail.com>
2023-11-03 07:26:22 -0300

    fix: Correção na páginação do html

--- codegroup-test/src/main/java/br/com/codegroup/codegrouptest/controllers/HomeController.java
+++ codegroup-test/src/main/java/br/com/codegroup/codegrouptest/controllers/HomeController.java
@@ -2,10 +2,8 @@ package br.com.codegroup.codegrouptest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
 public class HomeController {
     @GetMapping("/home")
     public String home() {
